54a286b15f6b5e93e2c9e5c8c934cedd46706eb6
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2023-11-12 12:25:04 -0800

    Updates
    Did not like how it was going so I am Restructuring it

--- webpholio/src/Pages/Refrences/Refrences.js
+++
@@ -1,8 +0,0 @@
-<div class="content">
-<p>Thierno Thiam</p>
-<a href="https://www.linkedin.com/in/thierno-thiam-9753489b/"> <img src="https://cdn1.iconfinder.com/data/icons/logotypes/32/circle-linkedin-512.png" alt="https://www.linkedin.com/in/thierno-thiam-9753489b/" height="2%" width="2%"></a>
-<p>Brian Koh</p> 
-<a href="https://www.linkedin.com/in/brian-koh-1a025819a"> <img src="https://cdn1.iconfinder.com/data/icons/logotypes/32/circle-linkedin-512.png" alt="https://www.linkedin.com/in/thierno-thiam-9753489b/" height="2%" width="2%"></a> 
-<p>Brandon Lomeli</p> 
-<a href="https://www.linkedin.com/in/brandon-lomeli-b6a5a626a"> <img src="https://cdn1.iconfinder.com/data/icons/logotypes/32/circle-linkedin-512.png" alt="https://www.linkedin.com/in/thierno-thiam-9753489b/" height="2%" width="2%"></a>
-</div>
