060005d56d592c8deb57c67f8f108f25620f1552
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2023-11-12 19:26:20 -0800

    Updates and changes

--- apps.js
+++ apps.js
@@ -4,15 +4,11 @@ function toggleAccordion() {
 
   const itemToggle = this.getAttribute('aria-expanded');
 
-  
-
   for (i = 0; i < items.length; i++) {
 
     items[i].setAttribute('aria-expanded', 'false');
 
   }
-
-  
 
   if (itemToggle == 'false') {
 
