72ca84729e33ef5496a90a7f15b95650113870eb
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2024-05-28 12:59:36 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 $(function(){
     setInterval(function(){
-      var utcdate = $('#utcdateformat');
-      var localdate = $('#localdateformat');  
+      const utcdate = $('#utcdateformat');
+      const localdate = $('#localdateformat');  
       //put UTC time into divUTC  
       utcdate.text(moment.utc().format('YYYY-MM-DD HH:mm:ss'));      
       utxtimes = moment.utc().format('YYYY-MM-DD HH:mm:ss');      
       //get text from divUTC and conver to local timezone  
-      var localTime  = moment.utc(utcdate.text()).toDate();
+      const localTime  = moment.utc(utcdate.text()).toDate();
       localTime = moment(localTime).format('YYYY-MM-DD HH:mm:ss');
       tms = moment(localTime).format("X");
       
-      $('#divstamp').text(tms);
+      $('#divstamp').text(tmsx);
       tmsx = moment(utxtimes).format("X");
       $('#divstampz').text(tmsx);
       //you can change tms to tmsx 
       //tms is localtime
       //tmsx is utctime
-      yournix = moment.unix(tms).format('DD-MM-YYYY HH:mm:ss')
+      yournix = moment.unix(tmsx).format('DD-MM-YYYY HH:mm:ss')
       localdate.text(localTime);   
        $('#divstampzclc').text(yournix);
     },1000);
