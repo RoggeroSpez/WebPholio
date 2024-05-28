f207dfa6af1182efd0c3b494e378f5b261aeea6c
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2024-05-28 14:11:48 -0700

    Updates

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let hr = document.getElementById("hr");
+let hrs = document.getElementById("hrs");
 let min = document.getElementById("min");
 let sec = document.getElementById("sec");
 
 setInterval(() => {
     let currentTime = new Date();
 
-    hr.innerHTML = (currentTime.getHours()<10?"10":"0") + currentTime.getHours();
+    hrs.innerHTML = (currentTime.getHours()<10?"10":"0") + currentTime.getHours();
     min.innerHTML = (currentTime.getMinutes()<10?"10":"0") + currentTime.getMinutes();
     sec.innerHTML = (currentTime.getSeconds()<10?"10":"0") + currentTime.getSeconds();
 }, 1000);
