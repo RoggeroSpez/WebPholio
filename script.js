e6764bb56cc2df5831b462c7500b96f39bcc8e22
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2024-05-28 12:56:20 -0700

    updates

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-const element =
-document.getElementById("time");
-
-setInteval(() => {
-    let date = new Date();
-    element.innerHTML = date.toLocaleTimeString();
-}, 1000);
+$(function(){
+    setInterval(function(){
+      var utcdate = $('#utcdateformat');
+      var localdate = $('#localdateformat');  
+      //put UTC time into divUTC  
+      utcdate.text(moment.utc().format('YYYY-MM-DD HH:mm:ss'));      
+      utxtimes = moment.utc().format('YYYY-MM-DD HH:mm:ss');      
+      //get text from divUTC and conver to local timezone  
+      var localTime  = moment.utc(utcdate.text()).toDate();
+      localTime = moment(localTime).format('YYYY-MM-DD HH:mm:ss');
+      tms = moment(localTime).format("X");
+      
+      $('#divstamp').text(tms);
+      tmsx = moment(utxtimes).format("X");
+      $('#divstampz').text(tmsx);
+      //you can change tms to tmsx 
+      //tms is localtime
+      //tmsx is utctime
+      yournix = moment.unix(tms).format('DD-MM-YYYY HH:mm:ss')
+      localdate.text(localTime);   
+       $('#divstampzclc').text(yournix);
+    },1000);
+  });
